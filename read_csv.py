e3c63d44673b63f8f2a79d3c3af973167a5ea53d
CastroRSJeev <castrorsjeev@gmail.com>
2026-03-22 17:34:48 +0530

    Removed testing files

--- read_csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('song_list.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    for i, row in enumerate(reader):
-        print(row)
-        if i >= 10:
-            break
--- read_csv_header.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('song_list.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    print(reader.fieldnames)
-    for i, row in enumerate(reader):
-        print(row)
-        if i >= 4:
-            break
